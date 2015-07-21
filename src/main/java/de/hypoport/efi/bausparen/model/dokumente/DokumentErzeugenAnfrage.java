23af87f99d3ff8b37a17cc1b75b2ad4e94c0051e
martin.klewitz <martin.klewitz@hypoport.de>
2015-07-21 17:50:29 +0200

    Domainmodell weiter

--- src/main/java/de/hypoport/efi/bausparen/model/dokumente/DokumentErzeugenAnfrage.java
+++ src/main/java/de/hypoport/efi/bausparen/model/dokumente/DokumentErzeugenAnfrage.java
@@ -1,8 +1,21 @@
 package de.hypoport.efi.bausparen.model.dokumente;
 
+import de.hypoport.efi.bausparen.model.berechnung.Abschlussgebuehrenbehandlung;
+import de.hypoport.efi.bausparen.model.berechnung.anfrage.SparBeitrag;
+
+import java.math.BigDecimal;
 import java.util.List;
 
 public class DokumentErzeugenAnfrage {
+
+  String tarif; //check
+
+  BigDecimal bausparSummeInEuro; //check
+  BigDecimal abzuloesendesDarlehenInEuro;
+
+  List<SparBeitrag> sparBeitraege;
+
+  Abschlussgebuehrenbehandlung abschlussgebuehrenbehandlung;
 
   List<Antragsteller> antragsteller;//check
 
@@ -10,6 +23,46 @@ public class DokumentErzeugenAnfrage {
   ZahlungsForm zahlungsForm;//check
 
   VermittlerDaten vermittlerDaten;//check
+
+  public String getTarif() {
+    return tarif;
+  }
+
+  public void setTarif(String tarif) {
+    this.tarif = tarif;
+  }
+
+  public BigDecimal getBausparSummeInEuro() {
+    return bausparSummeInEuro;
+  }
+
+  public void setBausparSummeInEuro(BigDecimal bausparSummeInEuro) {
+    this.bausparSummeInEuro = bausparSummeInEuro;
+  }
+
+  public BigDecimal getAbzuloesendesDarlehenInEuro() {
+    return abzuloesendesDarlehenInEuro;
+  }
+
+  public void setAbzuloesendesDarlehenInEuro(BigDecimal abzuloesendesDarlehenInEuro) {
+    this.abzuloesendesDarlehenInEuro = abzuloesendesDarlehenInEuro;
+  }
+
+  public List<SparBeitrag> getSparBeitraege() {
+    return sparBeitraege;
+  }
+
+  public void setSparBeitraege(List<SparBeitrag> sparBeitraege) {
+    this.sparBeitraege = sparBeitraege;
+  }
+
+  public Abschlussgebuehrenbehandlung getAbschlussgebuehrenbehandlung() {
+    return abschlussgebuehrenbehandlung;
+  }
+
+  public void setAbschlussgebuehrenbehandlung(Abschlussgebuehrenbehandlung abschlussgebuehrenbehandlung) {
+    this.abschlussgebuehrenbehandlung = abschlussgebuehrenbehandlung;
+  }
 
   public List<Antragsteller> getAntragsteller() {
     return antragsteller;
