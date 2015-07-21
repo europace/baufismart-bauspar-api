f37b749920813e6efe224148bfc1ae177a5f96a2
martin.klewitz <martin.klewitz@hypoport.de>
2015-07-21 16:22:00 +0200

    Strasse und Hausnummer getrennt

--- src/main/java/de/hypoport/efi/bausparen/model/dokumente/Adresse.java
+++ src/main/java/de/hypoport/efi/bausparen/model/dokumente/Adresse.java
@@ -2,8 +2,49 @@ package de.hypoport.efi.bausparen.model.dokumente;
 
 public class Adresse {
 
-  String strasseHausnummer;//check
+  String strasse;//check
+  String hausNummer;//check
   String postleitzahl;//check
   String wohnOrt;//check
   String landWohnsitz;//check
+
+  public String getStrasse() {
+    return strasse;
+  }
+
+  public void setStrasse(String strasse) {
+    this.strasse = strasse;
+  }
+
+  public String getHausNummer() {
+    return hausNummer;
+  }
+
+  public void setHausNummer(String hausNummer) {
+    this.hausNummer = hausNummer;
+  }
+
+  public String getPostleitzahl() {
+    return postleitzahl;
+  }
+
+  public void setPostleitzahl(String postleitzahl) {
+    this.postleitzahl = postleitzahl;
+  }
+
+  public String getWohnOrt() {
+    return wohnOrt;
+  }
+
+  public void setWohnOrt(String wohnOrt) {
+    this.wohnOrt = wohnOrt;
+  }
+
+  public String getLandWohnsitz() {
+    return landWohnsitz;
+  }
+
+  public void setLandWohnsitz(String landWohnsitz) {
+    this.landWohnsitz = landWohnsitz;
+  }
 }
