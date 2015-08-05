e3bc5f18e1d3ba199e0741deb16aedce0689070e
Marco Reimann <marco.reimann@hypoport.de>
2015-08-05 14:00:47 +0200

    KEFI-18131 Zahlplancheck um Saldenprüfung erweitert

--- src/test/java/de/hypoport/efi/bausparen/controller/FachlicheBeispiele.java
+++ src/test/java/de/hypoport/efi/bausparen/controller/FachlicheBeispiele.java
@@ -6,14 +6,17 @@ import de.hypoport.efi.bausparen.model.berechnung.Bausparangebot;
 import de.hypoport.efi.bausparen.model.berechnung.SparPlan;
 import de.hypoport.efi.bausparen.model.berechnung.SparPlanZahlung;
 import de.hypoport.efi.bausparen.model.berechnung.TilgungsPlan;
+import de.hypoport.efi.bausparen.model.berechnung.TilgungsPlanZahlung;
 import de.hypoport.efi.bausparen.model.berechnung.anfrage.BausparBerechnungsAnfrage;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static java.lang.String.format;
@@ -47,7 +50,6 @@ public class FachlicheBeispiele {
     }
     assertNotNull(bausparBerechnungsAnfrage.getDarlehensWunsch());
 
-    // TODO fachl. korrekte Assertions ergänzen
   }
 
   @Test
@@ -62,20 +64,49 @@ public class FachlicheBeispiele {
 
     assertEquals((long) berechnetesBausparAngebot.getGesamtlaufzeitKomplettInMonaten(), ermittleLaufzeit(berechnetesBausparAngebot));
 
-    pruefeSparplan(berechnetesBausparAngebot.getSparPhase().getSparPlan());
+    pruefeSparplan(berechnetesBausparAngebot.getSparPhase().getSparPlan(), berechnetesBausparAngebot.getAbschlussgebuehr().getAbschlussgebuehrBetragInEuro().negate());
     pruefeTilgungsplan(berechnetesBausparAngebot.getBausparDarlehen().getTilgungsPlan());
-    // TODO fachl. korrekte Assertions
 
   }
 
   private void pruefeTilgungsplan(TilgungsPlan tilgungsPlan) {
-    List<LocalDate> tilgungsDaten = tilgungsPlan.getZahlungen().stream().map(z -> z.getDatum()).collect(Collectors.toList());
+    List<TilgungsPlanZahlung> tilgungsPlanZahlungen = tilgungsPlan.getZahlungen();
+    List<LocalDate> tilgungsDaten = elementeAlsListe(tilgungsPlanZahlungen, z -> z.getDatum());
     assertZahlDatenAufeinanderfolgend(tilgungsDaten);
+
+    assertSaldenPassenZuZahlungen(
+        tilgungsPlanZahlungen, BigDecimal.ZERO,
+        z -> z.getZahlungInEuro().subtract(z.getZinsInEuro()),
+        z -> z.getSaldoNachZahlungInEuro()
+    );
   }
 
-  private void pruefeSparplan(SparPlan sparPlan) {
-    List<LocalDate> zahlDaten = sparPlan.getZahlungen().stream().map(z -> z.getDatum()).collect(Collectors.toList());
+  private void pruefeSparplan(SparPlan sparPlan, BigDecimal auszahlungsgebuehr) {
+    List<SparPlanZahlung> sparPlanZahlungen = sparPlan.getZahlungen();
+    List<LocalDate> zahlDaten = elementeAlsListe(sparPlanZahlungen, z -> z.getDatum());
     assertZahlDatenAufeinanderfolgend(zahlDaten);
+
+    assertSaldenPassenZuZahlungen(
+        sparPlanZahlungen, auszahlungsgebuehr,
+        z -> z.getZahlungInEuro().add(z.getZinsInEuro()),
+        z -> z.getSaldoNachZahlungInEuro()
+    );
+  }
+
+  private <Z> void assertSaldenPassenZuZahlungen(List<Z> zahlungsobjekte, BigDecimal anfangssaldo, Function<Z, BigDecimal> zahlungsErmittler, Function<Z, BigDecimal> saldoErmittler) {
+
+    assertTrue(zahlungsobjekte.size() > 0);
+    BigDecimal letzerSaldo = anfangssaldo;
+
+    for (int i = 0; i < zahlungsobjekte.size(); i++) {
+      Z zahlungsobjektI = zahlungsobjekte.get(i);
+      BigDecimal saldoNachZahlung = saldoErmittler.apply(zahlungsobjektI).setScale(2);
+      BigDecimal zahlung = zahlungsErmittler.apply(zahlungsobjektI);
+
+      assertEquals(format("Der %d. Saldo stimmt nicht überein:",i),saldoNachZahlung, letzerSaldo.add(zahlung));
+
+      letzerSaldo = saldoNachZahlung;
+    }
   }
 
   private void assertZahlDatenAufeinanderfolgend(List<LocalDate> zahlDaten) {
@@ -83,16 +114,24 @@ public class FachlicheBeispiele {
     LocalDate datumVorigeZahlung = zahlDaten.get(0);
     for (int i = 1; i < zahlDaten.size(); i++) {
       LocalDate datumZahlung = zahlDaten.get(i);
-      long monateZwischen = monateZwischen(datumVorigeZahlung, datumZahlung);
-      assertEquals(format("Die Daten %s und %s sollten aufeinander folgen",datumVorigeZahlung, datumZahlung),1, monateZwischen);
+      assertDatenFolgenAufeinander(datumVorigeZahlung, datumZahlung);
       datumVorigeZahlung = datumZahlung;
     }
+  }
+
+  private void assertDatenFolgenAufeinander(LocalDate datumVorigeZahlung, LocalDate datumZahlung) {
+    long monateZwischen = monateZwischen(datumVorigeZahlung, datumZahlung);
+    assertEquals(format("Die Daten %s und %s sollten aufeinander folgen", datumVorigeZahlung, datumZahlung), 1, monateZwischen);
   }
 
   private long ermittleLaufzeit(Bausparangebot berechnetesBausparAngebot) {
     SparPlanZahlung ersteSparrate = berechnetesBausparAngebot.getSparPhase().getSparPlan().getZahlungen().get(0);
     LocalDate tilgungsende = berechnetesBausparAngebot.getBausparDarlehen().getTilgungsende();
     return monateZwischen(ersteSparrate.getDatum(), tilgungsende);
+  }
+
+  private <S, D> List<D> elementeAlsListe(List<S> zahlungsListe, Function<S, D> f) {
+    return zahlungsListe.stream().map(f).collect(Collectors.toList());
   }
 
   private long monateZwischen(LocalDate erstesDatum, LocalDate zweitesDatumInklusive) {
