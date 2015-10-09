a66f908e9acfb484e1eb1fa01f4455643dc5582f
martin.klewitz <martin.klewitz@hypoport.de>
2015-10-09 11:31:52 +0200

    request logging

--- src/main/java/de/hypoport/efi/bausparen/controller/LoggingController.java
+++ src/main/java/de/hypoport/efi/bausparen/controller/LoggingController.java
@@ -1,10 +1,15 @@
 package de.hypoport.efi.bausparen.controller;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @ControllerAdvice(annotations = RestController.class)
 public class LoggingController {
@@ -12,7 +17,7 @@ public class LoggingController {
   private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(BausparBerechnungController.class);
 
   @ModelAttribute
-  public void addAttributes(@RequestBody String requestString) {
+  public void addAttributes(HttpServletRequest request, HttpServletResponse response,Model model, @RequestBody String requestString) {
     LOG.info("request=" + requestString);
   }
 }
